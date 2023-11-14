b232302c4c6cad8992986bdb1dcba571bfe9c1da
sarahmarkland <sarah.markland@icloud.com>
2023-11-14 12:11:53 -0600

    task 3

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -1,23 +1,26 @@
 //task 3 - closure and loops
 function createClassRoom(numbersOfStudents) {
   function studentSeat(seat) {
+    //return a func that returns the seat number
     return function() {
       return seat;
-    };
+    }
   }
 
+  //create and populate students array
   let students = [];
 
   for (let i = 0; i < numbersOfStudents; i++) {
-
+    //call studentSeat with current iteration+1
+    //and add return value to the students arr
     let seatNumber = i + 1;
     let student = studentSeat(seatNumber);
-    students.push(student());
+    students.push(student); //push the function (not the result of calling it)
   }
 
   return students;
 }
-// closure
+// closure classroom with 10 students
 let classRoom = createClassRoom(10);
 
 // console.log("Seat numbers of students in class:", classRoom);
